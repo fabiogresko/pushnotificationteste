fc84c64d91a978cd17f767eae0805554f336ba06
fabiogresko <fabiorsao@gmail.com>
2021-07-06 16:39:42 -0300

    ajuste notifications22

--- webpush.js
+++ webpush.js
@@ -13,6 +13,5 @@ webpush.setVapidDetails(
 );
 
 // This is the same output of calling JSON.stringify on a PushSubscription
-const pushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/e_KZqLKbxdI:APA91bELBIKOVUsCfSmKv7XEdl3h0FLNWFeR9xtoQa8NxPciILMId3TTUcsYRgHwrm3f2ENoYuFpbW8mNg8W0b0f-kT6U6WKG4VhlO_03JFTqaIQaMm_8mcszDAAbhdwlULODXTTsm36","expirationTime":null,"keys":{"p256dh":"BLWj4DatoH90hCO9xMhmpX4Nvtn-uO5JXa-ovUo82VSR3SOBc26Qog1z1mkxJfSDWb-tH5AmuVvAz3l0hVUGHQc","auth":"O2QT1Zo-0hOhLnj_la93Qg"}};
-
+const pushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/fapt-Rcy0q4:APA91bEoaz1w0_KOQ4HItw9WJMGtyfbzL3K7oXAhkZ3jtDSIV3W_LPXLjMyBph57xXU-Vfz6nhzVW0FBi6Nhjk7x1ZXNOsKxhbcJlQlj3w3T_tblwdlfnLsWKwa1KM4o-CSSEybyJZ0w","expirationTime":null,"keys":{"p256dh":"BB4LJSkVKMjuiS8885ZF8PqibyutvJsF6SmhnjbMqk76uOU04ZJo-FOLxwWF-7s72UHUUdIcVlBFshNU3VIPQBM","auth":"26lXXujFM84FSYfxXbxOpQ"}};
 webpush.sendNotification(pushSubscription, 'TECH4HUMANS - FABIO');
