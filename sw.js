8a719b6df3856406f3d02a99da679f9bdbcd463d
fabiogresko <fabiorsao@gmail.com>
2021-07-06 16:10:45 -0300

    ajuste notifications

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-self.addEventListener('push', () => {
-  self.registration.showNotification('Hello world!', options);
+self.addEventListener('push', function(e) {
+  var options = {
+    body: 'Notificação de teste por Fabio!',
+    icon: 'images/example.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2'
+    },
+    actions: [
+      {action: 'explore', title: 'Explore this new world',
+        icon: 'images/checkmark.png'},
+      {action: 'close', title: 'Close',
+        icon: 'images/xmark.png'},
+    ]
+  };
+  e.waitUntil(
+    self.registration.showNotification('Tech4Humans!', options)
+  );
 });
--- webpush.js
+++ webpush.js
@@ -13,12 +13,6 @@ webpush.setVapidDetails(
 );
 
 // This is the same output of calling JSON.stringify on a PushSubscription
-const pushSubscription = {
-  endpoint: '.....',
-  keys: {
-    auth: '.....',
-    p256dh: '.....'
-  }
-};
+const pushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/fapt-Rcy0q4:APA91bEoaz1w0_KOQ4HItw9WJMGtyfbzL3K7oXAhkZ3jtDSIV3W_LPXLjMyBph57xXU-Vfz6nhzVW0FBi6Nhjk7x1ZXNOsKxhbcJlQlj3w3T_tblwdlfnLsWKwa1KM4o-CSSEybyJZ0w","expirationTime":null,"keys":{"p256dh":"BB4LJSkVKMjuiS8885ZF8PqibyutvJsF6SmhnjbMqk76uOU04ZJo-FOLxwWF-7s72UHUUdIcVlBFshNU3VIPQBM","auth":"26lXXujFM84FSYfxXbxOpQ"}};
 
 webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
